6929dbdf3b8c30b348a83cd0c1517255cef0deb5
Lautaro Silva <lautaro.e.silva03@gmail.com>
2026-09-14 10:29:47 -0300

    notas sobre cómo usar estados

--- Empanadas Shaan/Scripts/personaje/estados/Idle.gd
+++ Empanadas Shaan/Scripts/personaje/estados/Idle.gd
@@ -1,6 +1,13 @@
 extends Estados_de_jugador
 #Lógica del estado de Idle
 
+#Cada estado debe tener 2 partes: 
+#	1: su funcionamiento lógico
+#	2: su cambio hacia otro estado
+
+#debe tener los métodos de procesamiento comunes pero seguidos de un "on_",
+#de esta manera, se asegura de que el unico proceso consultando cosas 60 veces x frame
+#sea el de la máquina de estados
 func on_physics_process(delta):
 	handle_gravity(delta)
 
